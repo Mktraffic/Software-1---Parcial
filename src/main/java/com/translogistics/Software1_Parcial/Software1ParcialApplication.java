e53208887982ee47e9372fc4b535f6bea3ab9e84
David Lotero <Davidloteria@gmail.com>
2025-03-08 23:12:56 -0500

    Configuración inicial del proyecto Software1_Parcial

---
+++ src/main/java/com/translogistics/Software1_Parcial/Software1ParcialApplication.java
@@ -0,0 +1,13 @@
+package com.translogistics.Software1_Parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Software1ParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Software1ParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/translogistics/Software1_Parcial/Software1ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translogistics.Software1_Parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Software1ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
